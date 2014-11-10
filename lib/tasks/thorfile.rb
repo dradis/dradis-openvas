2967806a12bc7c6523a983ea8cb552e3cba95e1b
Daniel Martin <etd-github@nomejortu.com>
2014-11-10 13:05:36 +0100

    Temporary tweaks to Thorfile

--- lib/tasks/thorfile.rb
+++ lib/tasks/thorfile.rb
@@ -1,5 +1,6 @@
 class DradisTasks < Thor
   class Upload < Thor
+include Core::Pro::ProjectScopedTask
     namespace     "dradis:upload"
 
     desc "openvas FILE", "upload OpenVAS results"
@@ -14,9 +15,25 @@ class DradisTasks < Thor
         exit -1
       end
 
-      OpenvasUpload.import(
-        :file => file_path,
-        :logger => logger)
+detect_and_set_project_scope
+
+      content_service = nil
+      template_service = nil
+      if defined?(Dradis::Pro)
+        content_service = Dradis::Pro::Plugins::ContentService.new(plugin: Dradis::Plugins::Acunetix)
+        template_service = Dradis::Pro::Plugins::TemplateService.new(plugin: Dradis::Plugins::Acunetix)
+      else
+        content_service = Dradis::Plugins::ContentService.new(plugin: Dradis::Plugins::Acunetix)
+        template_service = Dradis::Plugins::TemplateService.new(plugin: Dradis::Plugins::Acunetix)
+      end
+
+      importer = Dradis::Plugins::Acunetix::Importer.new(
+                  logger: logger,
+         content_service: content_service,
+        template_service: template_service
+      )
+
+      importer.import(file: file_path)
 
       logger.close
     end
