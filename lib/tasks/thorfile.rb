a49c6d9f6e449878ce25e65b22b65a38707661fd
Daniel Martin <etd-github@nomejortu.com>
2014-11-10 16:09:36 +0100

    Thorfile - remove references to Acunetix (c&p error)

--- lib/tasks/thorfile.rb
+++ lib/tasks/thorfile.rb
@@ -1,42 +1,47 @@
-class DradisTasks < Thor
-  class Upload < Thor
+module Dradis
+  module Plugins
+    module OpenVAS
+      class Thor < Thor
 include Core::Pro::ProjectScopedTask
-    namespace     "dradis:upload"
 
-    desc "openvas FILE", "upload OpenVAS results"
-    def openvas(file_path)
-      require 'config/environment'
+        namespace     "dradis:plugins:openvas"
 
-      logger = Logger.new(STDOUT)
-      logger.level = Logger::DEBUG
+        desc "upload FILE", "upload OpenVAS XML results"
+        def upload(file_path)
+          require 'config/environment'
 
-      unless File.exists?(file_path)
-        $stderr.puts "** the file [#{file_path}] does not exist"
-        exit -1
-      end
+          logger = Logger.new(STDOUT)
+          logger.level = Logger::DEBUG
+
+          unless File.exists?(file_path)
+            $stderr.puts "** the file [#{file_path}] does not exist"
+            exit -1
+          end
 
 detect_and_set_project_scope
 
-      content_service = nil
-      template_service = nil
-      if defined?(Dradis::Pro)
-        content_service = Dradis::Pro::Plugins::ContentService.new(plugin: Dradis::Plugins::Acunetix)
-        template_service = Dradis::Pro::Plugins::TemplateService.new(plugin: Dradis::Plugins::Acunetix)
-      else
-        content_service = Dradis::Plugins::ContentService.new(plugin: Dradis::Plugins::Acunetix)
-        template_service = Dradis::Plugins::TemplateService.new(plugin: Dradis::Plugins::Acunetix)
+          content_service = nil
+          template_service = nil
+          if defined?(Dradis::Pro)
+            content_service = Dradis::Pro::Plugins::ContentService.new(plugin: Dradis::Plugins::OpenVAS)
+            template_service = Dradis::Pro::Plugins::TemplateService.new(plugin: Dradis::Plugins::OpenVAS)
+          else
+            content_service = Dradis::Plugins::ContentService.new(plugin: Dradis::Plugins::OpenVAS)
+            template_service = Dradis::Plugins::TemplateService.new(plugin: Dradis::Plugins::OpenVAS)
+          end
+
+          importer = Dradis::Plugins::OpenVAS::Importer.new(
+                      logger: logger,
+             content_service: content_service,
+            template_service: template_service
+          )
+
+          importer.import(file: file_path)
+
+          logger.close
+        end
+
       end
-
-      importer = Dradis::Plugins::Acunetix::Importer.new(
-                  logger: logger,
-         content_service: content_service,
-        template_service: template_service
-      )
-
-      importer.import(file: file_path)
-
-      logger.close
     end
-
   end
 end
