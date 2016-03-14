3315a15e8340561eea5885db0dbae1ae830ae80c
Daniel Martin <etd-github@nomejortu.com>
2016-03-14 12:53:11 +0100

    We no longer need a Meta module

--- lib/dradis/plugins/openvas.rb
+++ lib/dradis/plugins/openvas.rb
@@ -9,19 +9,3 @@ require 'dradis/plugins/openvas/engine'
 require 'dradis/plugins/openvas/field_processor'
 require 'dradis/plugins/openvas/importer'
 require 'dradis/plugins/openvas/version'
-
-# This is required while we transition the Upload Manager to use
-# Dradis::Plugins only
-module Dradis
-  module Plugins
-    module OpenVAS
-      module Meta
-        NAME = "OpenVAS XML upload plugin"
-        EXPECTS = "OpenVAS XML v6 or v7 format."
-        module VERSION
-          include Dradis::Plugins::OpenVAS::VERSION
-        end
-      end
-    end
-  end
-end
