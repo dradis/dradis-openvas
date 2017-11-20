770a2f09fc19e767b35b75a583fb8efa6ffb4a36
Rachael Korinek <rachael.korinek@gmail.com>
2017-11-20 15:32:30 -0600

    Update specs

--- spec/openvas/result_spec.rb
+++ spec/openvas/result_spec.rb
@@ -1,35 +1,61 @@
 require 'spec_helper'
 
-describe Openvas::Result do
-  include FixtureLoader
+  describe 'OpenVAS upload plugin' do
+    before(:each) do
+      # Stub template service
+      templates_dir = File.expand_path('../../../templates', __FILE__)
+      expect_any_instance_of(Dradis::Plugins::TemplateService)
+      .to receive(:default_templates_dir).and_return(templates_dir)
 
-  it "splits the <description> tag in its component fields" do
-    xml_doc = load_fixture_file('result.xml')
-    result = Openvas::Result.new( xml_doc.at_xpath('/result') )
-    result.description.should eq(xml_doc.at_xpath('/result/description').text)
+      # Init services
+      plugin = Dradis::Plugins::OpenVAS
 
-    expect(result.summary).to eq("This host is installed with Oracle Java SE JRE and is prone to\nmultiple vulnerabilities.\n\n")
-    expect(result.insight).to eq("Multiple flaws are caused by unspecified errors in the following\ncomponents:\n- 2D\n- AWT\n- Sound\n- I18n\n- CORBA\n- Serialization\n\n")
+      @content_service = Dradis::Plugins::ContentService::Base.new(
+        logger: Logger.new(STDOUT),
+        plugin: plugin
+      )
+
+      @importer = Dradis::Plugins::OpenVAS::Importer.new(
+        content_service: @content_service
+      )
+
+      # Stub dradis-plugins methods
+      #
+      # They return their argument hashes as objects mimicking
+      # Nodes, Issues, etc
+      allow(@content_service).to receive(:create_node) do |args|
+        obj = OpenStruct.new(args)
+        obj.define_singleton_method(:set_property) { |_, __| }
+        obj
+      end
+      allow(@content_service).to receive(:create_issue) do |args|
+        OpenStruct.new(args)
+      end
+      allow(@content_service).to receive(:create_evidence) do |args|
+        OpenStruct.new(args)
+      end
+    end
+
+    it "creates issues and evidence as expected" do
+      # splits the <tags> into the correct component fields
+      # respects paragraphs within the component fields of <tags>
+      expect(@content_service).to receive(:create_issue) do |args|
+        expect(args[:text]).to include("#[Title]#\nOpenSSL CCS Man in the Middle Security Bypass Vulnerability")
+        expect(args[:text]).to include("#[Description]#\nOpenSSL is prone to security-bypass vulnerability.")
+        expect(args[:text]).to include("#[Recommendation]#\nUpdates are available.")
+        expect(args[:text]).to include("#[Insight]#\nOpenSSL does not properly restrict processing of ChangeCipherSpec\nmessages, which allows man-in-the-middle attackers to trigger use of a\nzero-length master key in certain OpenSSL-to-OpenSSL communications, and\nconsequently hijack sessions or obtain sensitive information, via a crafted\nTLS handshake, aka the 'CCS Injection' vulnerability.")
+        expect(args[:text]).to include("#[Detection]#\nSend two SSL ChangeCipherSpec request and check the response.")
+        OpenStruct.new(args)
+      end
+      # pulls threat and severity down to the Evidence level
+      expect(@content_service).to receive(:create_evidence) do |args|
+        expect(args[:content]).to include("#[Threat]#\nMedium")
+        expect(args[:content]).to include("#[Severity]#\n6.8")
+        expect(args[:issue].text).to include("#[Title]#\nOpenSSL CCS Man in the Middle Security Bypass Vulnerability")
+        expect(args[:node].label).to eq("10.10.10.10")
+      end
+
+      @importer.import(file: 'spec/fixtures/files/v7/report_v7.xml')
+    end
+
   end
-
-  it "respects paragraphs within the component fields of the <description> value" do
-    xml_doc = load_fixture_file('result2.xml')
-    result = Openvas::Result.new( xml_doc.at_xpath('/result') )
-    result.summary.should eq("A weakness has been discovered in Apache web servers that are\nconfigured to use the FileETag directive. Due to the way in which\nApache generates ETag response headers, it may be possible for an\nattacker to obtain sensitive information regarding server files.\nSpecifically, ETag header fields returned to a client contain the\nfile's inode number.\n\nExploitation of this issue may provide an attacker with information\nthat may be used to launch further attacks against a target network.\n\nOpenBSD has released a patch that addresses this issue. Inode numbers\nreturned from the server are now encoded using a private hash to avoid\nthe release of sensitive information.\n")
-  end
-
-  it "correctly parses the fringe 'Impact Level' case" do
-    xml_doc = load_fixture_file('result.xml')
-    result = Openvas::Result.new( xml_doc.at_xpath('/result') )
-
-    result.impact_level.should eq('System/Application')
-  end
-
-
-  it "correctly parses the last component field in the <description>" do
-    xml_doc = load_fixture_file('result2.xml')
-    result = Openvas::Result.new( xml_doc.at_xpath('/result') )
-
-    result.info_gathered.should eq("Inode: 1050855\nSize: 177\n\n")
-  end
-end
