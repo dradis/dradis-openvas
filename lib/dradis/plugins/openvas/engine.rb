c3b1dd1b5f8139d73138c1b5ba39920572e2b5d3
Daniel Martin <53006+etdsoft@users.noreply.github.com>
2021-02-01 10:04:18 +0100

    Improve syntax to please Rails 6's loader

--- lib/dradis/plugins/openvas/engine.rb
+++ lib/dradis/plugins/openvas/engine.rb
@@ -5,5 +5,11 @@ module Dradis::Plugins::OpenVAS
     include ::Dradis::Plugins::Base
     description 'Processes OpenVAS XML v6 or v7 format'
     provides :upload
+
+    initializer "openvas.inflections" do |app|
+      ActiveSupport::Inflector.inflections do |inflect|
+        inflect.acronym('OpenVAS')
+      end
+    end
   end
 end
