7822004f129e5b00a0d2dd59db7aaba6770d2a73
Matt Budz <matt.budz@protonmail.com>
2024-03-18 19:33:13 +0100

    use constant for default mappings

--- lib/dradis/plugins/openvas/mapping.rb
+++ lib/dradis/plugins/openvas/mapping.rb
@@ -1,25 +1,23 @@
 module Dradis::Plugins::OpenVAS
   module Mapping
-    def self.default_mapping
-      {
-        'evidence' => {
-          'Port' => '{{ openvas[evidence.port] }}',
-          'Description' => '{{ openvas[evidence.description] }}'
-        },
-        'result' => {
-          'Title' => '{{ openvas[result.name] }}',
-          'CVSSv2' => '{{ openvas[result.cvss_base] }}',
-          'AffectedSoftware' => '{{ openvas[result.affected_software] }}',
-          'Description' => '{{ openvas[result.summary] }}',
-          'Recommendation' => '{{ openvas[result.solution] }}',
-          'References' => "CVE: {{ openvas[result.cve] }}\n
-                          CVSS Vector: {{ cvss_base_vector }}\n
-                          BID: {{ openvas[result.bid] }}\n
-                          Other: {{ openvas[result.xref] }}",
-          'RawDescription' => "(note that some of the information below can change from instance to instance of this problem)\n
-                              {{ openvas[result.description] }}"
-        }
+    DEFAULT_MAPPING = {
+      evidence: {
+        'Port' => '{{ openvas[evidence.port] }}',
+        'Description' => '{{ openvas[evidence.description] }}'
+      },
+      result: {
+        'Title' => '{{ openvas[result.name] }}',
+        'CVSSv2' => '{{ openvas[result.cvss_base] }}',
+        'AffectedSoftware' => '{{ openvas[result.affected_software] }}',
+        'Description' => '{{ openvas[result.summary] }}',
+        'Recommendation' => '{{ openvas[result.solution] }}',
+        'References' => "CVE: {{ openvas[result.cve] }}\n
+                        CVSS Vector: {{ cvss_base_vector }}\n
+                        BID: {{ openvas[result.bid] }}\n
+                        Other: {{ openvas[result.xref] }}",
+        'RawDescription' => "(note that some of the information below can change from instance to instance of this problem)\n
+                            {{ openvas[result.description] }}"
       }
-    end
+    }.freeze
   end
 end
